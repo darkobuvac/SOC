83575472858342c1c2d8be8b70e518b81ae79783
darkobuvac <darko.buvac@codaxy.com>
2023-07-16 09:54:41 +0200

    setup conductor sharp on scanning microservice

---
+++ server/SOC.Scanning/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SOC.Scanning.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddAutoMapper(typeof(Program).Assembly);
+
+        return services;
+    }
+}
